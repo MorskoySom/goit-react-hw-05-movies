8ec97928f032479b671cbf495822b9aa30103fd8
MorskoySom <Somovvitvit@gmail.com>
2023-10-26 16:56:12 +0300

    109

--- src/api.js
+++ src/api.js
@@ -10,12 +10,21 @@ export const trandsDay = async () => {
     return response.data
 }
 
-export const searchMovie = async (title) => {
+export const searchMovieTitle = async (title) => {
     const params = new URLSearchParams({
         api_key: API_KEY,
         query: title,
     });
     const response = await axios.get(`https://api.themoviedb.org/3/search/movie?${params}`);
+    return response.data
+}
+
+export const searchMovieId = async (movieID) => {
+    const movie_id = movieID
+    const params = new URLSearchParams({
+        api_key: API_KEY,
+    });
+    const response = await axios.get(`https://api.themoviedb.org/3/movie/${movie_id}?${params}`);
     return response.data
 }
 
--- src/pages/MoviesPage.js
+++ src/pages/MoviesPage.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { searchMovie } from "../api";
+import { searchMovieTitle } from "../api";
 
 export default function MoviesPage() {
     const [movieData, setMovieData] = useState(null);
     const { title } = useParams();
 
     useEffect(() => {
-        searchMovie(title)
+        searchMovieTitle(title)
             .then(data => {
                 setMovieData(data);
                 console.log(data);
@@ -24,11 +24,10 @@ export default function MoviesPage() {
     return (
         <div>
             <h1>{movieData.original_title}</h1>
-            <p>{movieData.overview}</p>
+
         </div>
     );
 }
-
 
 // export default function MoviesPage() {
 //     return <h1> MoviesPage </h1>
